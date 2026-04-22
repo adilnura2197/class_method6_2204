cd633ed26a6d777efcfb41d414f23a708e66d582
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2026-04-22 16:56:54 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+class Mashina:
+    def start(self):
+        print("Mashina ishga tushdi")
+
+    def drive(self):
+        self.start()
+        print("Mashina harakatlandi")
+
+    def stop(self):
+        self.drive()
+        print("Mashina to‘xtadi")
